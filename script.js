7af504abc29bf0a48c0fda69d277ba0dcf1ad86f
jesus Gomez <94jesusgomez@gmail.com>
2022-03-21 17:39:17 +0000

    final proyect

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate =document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if( (principal=="") ){
+        alert("Please enter deposit");
+        this.principal.focus();
+        return false;
+    }
+
+
+    if( (principal < 0 ) ){
+        alert("Please Enter a positive number");
+        this.principal.focus();
+        return false;
+    }
+
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear()+ parseInt(years);
+
+    document.getElementById("result").innerText=
+    'If you deposit ' + principal + ', \n' +
+    'at an interest rate of ' + rate + '%.\n' +
+    'You will receive an amount of ' + interest + ', \n' +
+    'in the year ' + year
 }
-        
+
+ function updateRate() {
+        var rateval = document.getElementById("rate").value;
+
+        document.getElementById("rate_val").innerText=rateval;
+ }
